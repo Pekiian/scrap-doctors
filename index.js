df53a214b01a48f4812c0d07930bfcd55b55ab94
Pekiian <prchiantore@gmail.com>
2024-04-06 22:11:30 -0300

    Arreglado

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT
+const PORT = 4000
 const puppeteer = require('puppeteer')
 
 async function openWebPage(dni) {
-    const browser = await puppeteer.launch({
-        args: [
-              "--disable-setuid-sandbox",
-              "--no-sandbox",
-              "--single-process",
-              "--no-zygote",
-            ],
-            executablePath:
-              process.env.NODE_ENV === "production"
-                ? process.env.PUPPETEER_EXECUTABLE_PATH
-                : puppeteer.executablePath(),
-    });
-    const page = await browser.newPage();
-    await page.goto("https://sisa.msal.gov.ar/sisa/");
-    await page.click('.ico_mod_agenda')
-    await page.click('.util_textsize32.icon-reg_refeps')
-    await page.type('.gwt-TextBox:nth-child(2)', dni)
-    await page.click('.GGGX03MKU>.util_rec_alcent>a>.boton')
-    await page.waitForSelector('.texto3 > td.cell:nth-of-type(4)')
-    let element = await page.$('.texto3 > td.cell:nth-of-type(4)')
-    let value = await page.evaluate(el => el.textContent, element)
-    await browser.close();
-
-    return value
+    try{
+        const browser = await puppeteer.launch({
+            args: [
+                  "--disable-setuid-sandbox",
+                  "--no-sandbox",
+                  "--single-process",
+                  "--no-zygote",
+                ],
+                executablePath:
+                  process.env.NODE_ENV === "production"
+                    ? process.env.PUPPETEER_EXECUTABLE_PATH
+                    : puppeteer.executablePath(),
+        });
+        const page = await browser.newPage();
+        await page.goto("https://sisa.msal.gov.ar/sisa/");
+        await page.click('.ico_mod_agenda')
+        await page.click('.util_textsize32.icon-reg_refeps')
+        await page.type('.gwt-TextBox:nth-child(2)', dni)
+        await page.click('.GGGX03MKU>.util_rec_alcent>a>.boton')
+        await page.waitForSelector('.texto3 > td.cell:nth-of-type(4)', { timeout: 5000 })
+        let element = await page.$('.texto3 > td.cell:nth-of-type(4)')
+        let value = await page.evaluate(el => el.textContent, element)
+        await browser.close();
+    
+        return value
+    } catch(e){
+        return undefined
+    }
 }
 
 app.get("/", (req, res) => {
@@ -39,10 +43,10 @@ app.get('/getDoctor/:dni', async function (req, res) {
     const value = await openWebPage(dni)
 
     if(!value){
-        res.send(404)
+        res.sendStatus(404)
+    } else{
+        res.sendStatus(204)
     }
-
-    res.send(204)
 })
  
 app.listen(PORT, function (err) {
